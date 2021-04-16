273501f29be6755c6f0b05377275d9bc2396916e
Arntje <a.braakenburg@chello.nl>
2021-04-16 12:07:37 +0200

    opgelost

--- math-methods.js
+++ math-methods.js
@@ -8,19 +8,20 @@ console.log(Math.PI);
 // Bereken de omtrek van een cirkel met een diameter van 4 en log dit in de terminal. De eenheid (cm, meters, etc. maakt niet uit).
 // Tip: de formule is omtrek = diameter * pi
 // ---- Verwachte uitkomst: 12.566370614359172
-
+const Circumference4 = Math.PI * 4;
+console.log(Circumference4);
 
 /* Opdracht 2: */
 // Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar een heel getal. Log dit in de terminal.
 // ---- Verwachte uitkomst: 38
-
-
+const Circumreference12 = Math.round(Math.PI * 12);
+console.log(Circumreference12);
 
 /* Opdracht 3: */
 // Bereken de wortel van 9 en log dit in de terminal
 // ---- Verwachte uitkomst: 3
-
-
+const squareRoot9 = Math.sqrt(9);
+console.log(squareRoot9);
 
 /* Opdracht 4: */
 // Genereer een willekeurig getal tussen de 0 en 1 en doe dan een check:
@@ -28,3 +29,26 @@ console.log(Math.PI);
 // * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
 // Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+let getNumber = Math.random();
+console.log(getNumber);
+if (getNumber > 0.5)    {
+    console.log("Het glas is halfvol");
+} else  {
+    console.log("Het glas is half leeg" + "\n")
+}
+
+getNumber = Math.random();
+console.log(getNumber);
+if (getNumber > 0.5)    {
+    console.log("Het glas is halfvol");
+} else  {
+    console.log("Het glas is half leeg" + "\n")
+}
+
+getNumber = Math.random();
+console.log(getNumber);
+if (getNumber > 0.5)    {
+    console.log("Het glas is halfvol");
+} else  {
+    console.log("Het glas is half leeg" + "\n")
+}
--- string-methods.js
+++ string-methods.js
@@ -9,41 +9,42 @@ console.log(word.length);
 /* Opdracht 1 */
 //  Check of het woord "Banaan" langer is dan het woord "Aardappel" en log dit in de terminal.
 // ---- Verwachte uitkomst: false
-
 const fruit = "Banaan";
 const vegetable = "Aardappel";
-
-
+console.log(fruit.length>vegetable.length);
 
 /* Opdracht 2 */
 //  Zorg ervoor dat de laatste letter van het woord in fruit en vegetable (hierboven) in de terminal gelogd wordt.
 //  Tip: bedenk 1 manier die voor beide woorden zou werken.
 // ---- Verwachte uitkomst voor fruit: "n"
 // ---- Verwachte uitkomst voor vegetable: "l"
-
-
+const lastLetterOfFruit = fruit.substring(fruit.length-1);
+const lastLetterOfVegetable = vegetable.substring((vegetable.length-1))
+console.log(lastLetterOfFruit + " " + lastLetterOfVegetable);
 
 /* Opdracht 3 */
 //  Haal de spaties aan het begin en eind van onderstaande zin weg en log dit in de terminal
 // ---- Verwachte uitkomst: "De kat krabt de krullen van de trap"
 
 const userInput = "  De kat krabt de krullen van de trap ";
-
-
+newUserInput = userInput.trim();
+console.log(newUserInput);
 
 /* Opdracht 4 */
 // Check of het woord "Fantastisch" in onderstaande zin voorkomt en log dit in de terminal.
 // ---- Verwachte uitkomst: true
 
 const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
-
-
+let includesFantastisch = story.includes("Fantastisch");
+console.log(includesFantastisch);
 
 /* Opdracht 5 */
 // Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
 // ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
+const endOfSentence = story.indexOf('.');
 
-
+const sentence = story.substring(0, endOfSentence + 1);
+console.log(sentence);
 
 /* Opdracht 6 */
 // Maak van onderstaand bedrag een Nederlands bedrag door het volgende te doen:
@@ -53,7 +54,8 @@ const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch,
 // ---- Verwachte uitkomst: "€35,59"
 
 const price = "$35.59";
-
+const dutchPrice = price.replace("$", "€").replace(",", ".");
+console.log(dutchPrice);
 
 
 
